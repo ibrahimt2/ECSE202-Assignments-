79b6b851f250c9c7168932ac02ec81582bc4d1c3
ibrahimt2 <zerobottles@outlook.com>
2019-01-19 17:29:11 -0500

    Update bSim.java

--- ECSE202 Assignment2/bSim.java
+++ ECSE202 Assignment2/bSim.java
@@ -28,26 +28,26 @@ public class bSim extends GraphicsProgram {
 	
 	public void run() {
 		this.resize(WIDTH, HEIGHT);						//Adjusts canvas dimensions
-		BallArray = new gBall[NUMBALLS]; 				//Creating a gBall array with 100 spaces
+		BallArray = new gBall[NUMBALLS]; 					//Creating a gBall array with 100 spaces
 		
 		GLine Ground = new GLine(GROUND_BEGIN, HEIGHT - OFFSET, GROUND_END, HEIGHT - OFFSET);   	//Creates the ground
 		add(Ground); 
 		
-		for (int i = 0; i < BallArray.length; i++) { 	//Iterates over the array, and creates 100 gBalls
+		for (int i = 0; i < BallArray.length; i++) { 		//Iterates over the array, and creates 100 gBalls
 			double Xi = rgen.nextDouble(XMIN, XMAX); 	//Random generation of variables values within given limits
 			double Yi = rgen.nextDouble(YMIN, YMAX);
 			double bSize = rgen.nextDouble(MINSIZE, MAXSIZE);
 			Color bColor = rgen.nextColor();
 			double bLoss = rgen.nextDouble(EMIN, EMAX);
-			double bVel = rgen.nextDouble(VMIN, VMAX); //The six adjusts for metres
+			double bVel = rgen.nextDouble(VMIN, VMAX); 	//The six adjusts for metres
 			
 			BallArray[i] = new gBall(Xi, Yi, bSize, bColor, bLoss, bVel); //Creates a gBall with given values and stores it in the BallArray
 			
 		}
 		
-		for (int i = 0; i < BallArray.length; i++) {	//Iterating over array again
+		for (int i = 0; i < BallArray.length; i++) {			//Iterating over array again
 			add(BallArray[i].BouncingBall);				//Adds a ball from the Array onto the canvas
-			BallArray[i].start();						//Starts the thread that controls the physics of the ball and changes it's location in gBall Class
+			BallArray[i].start();					//Starts the thread that controls the physics of the ball and changes it's location in gBall Class
 		}
 	}
 	
